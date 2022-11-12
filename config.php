f942acda596984b173e4b9fbd097a62a95672d1f
rabiulhassandev <rabiulhassandev@gmail.com>
2022-11-12 21:29:09 +0600

    gitignore file added

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost","root","","income_expense");
+$con = mysqli_connect("localhost","root","","dailyexpense");
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
